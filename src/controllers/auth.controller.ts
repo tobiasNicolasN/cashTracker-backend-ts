ed08ca3b76017ff96c448b6d3840d216d52327cb
Tobias <tobias.nicolas001@gmail.com>
2024-01-17 11:39:23 -0300

    Logout controller finished

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -46,8 +46,14 @@ export const login = async (req: Request, res: Response) => {
       createdAt: userFound.createdAt,
       updatedAt: userFound.updatedAt,
     });
-    
   } catch (error) {
     res.status(500).json({ message: error });
   }
+};
+
+export const logout = (req: Request, res: Response) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 };
