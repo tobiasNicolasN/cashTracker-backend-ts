c39de12206f0255b68b2f3e6b354baadffdbe476
TobiasNicolasN <tobias.nicolas001@gmail.com>
2024-02-19 14:35:40 -0300

    Cors added

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import authRoutes from "./routes/auth.routes";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import expensesRoutes from './routes/expense.routes'
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors())
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
